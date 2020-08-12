231d0397c21a7d6b7c019574417139790e85abb1
jyh7a <jyh7a@naver.com>
2020-08-13 03:21:02 +0900

    sfc

---
+++ learn-vue-js-master/vue-cli/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  
+  <div>
+    <!-- <app-header></app-header> -->
+    <!-- <AppHeader v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"/> -->
+    <AppHeader
+     v-bind:propsdata="str"
+     v-on:renew="renewStr"
+     />
+
+  </div>
+
+
+</template>
+
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  data: function() {
+    return {
+      str: 'Header'
+    }
+  },
+  components: {
+    // 'app-header' : AppHeader
+    AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hi'
+    }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
