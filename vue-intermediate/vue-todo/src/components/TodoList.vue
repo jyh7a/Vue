877690247f9b4ab677a95d0016ce3cf70b07584a
jyh7a <jyh7a@naver.com>
2020-08-14 04:38:58 +0900

    create li

---
+++ vue-intermediate/vue-todo/src/components/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+        {{ todoItem }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data: function() {
+    return {
+      todoItems: []
+    }
+  },
+  created: function() {
+    if(localStorage.length > 0){
+      for (let i=0; i<localStorage.length; i++){
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+          this.todoItems = [...this.todoItems, localStorage.key(i)]
+        } 
+      }
+    }    
+  }
+}
+</script>
+
+
+<style>
+
+</style>
