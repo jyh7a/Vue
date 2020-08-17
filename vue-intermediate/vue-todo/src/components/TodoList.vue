843b7953f52570d6824d567c8de5c0d1bb488005
jyh7a <jyh7a@naver.com>
2020-08-18 03:05:33 +0900

    Update TodoList.vue

--- vue-intermediate/vue-todo/src/components/TodoList.vue
+++ vue-intermediate/vue-todo/src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
 
         <i class="fas fa-check checkBtn" 
@@ -12,7 +12,7 @@
         </span>
 
       </li>
-    </ul>
+    </transition-group>  
   </div>
 </template>
 
@@ -77,5 +77,12 @@ li{
 }
 
 
-
+/* list item transition effect */
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>
