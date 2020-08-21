66dd3086a54e118cc3bc718d87dda743bfc1faab
jyh7a <jyh7a@naver.com>
2020-08-21 21:56:42 +0900

    vue helper

--- vue-intermediate/test/test.js
+++ vue-intermediate/test/test.js
@@ -2,8 +2,11 @@
 // a.num = 10
 // console.log(a)
 
-const a = []
-a.push(20)
-console.log(a)
+// const a = []
+// a.push(20)
+// console.log(a)
 
- 
+let josh = {
+  field: 'web',
+  language: 'js'
+} 
